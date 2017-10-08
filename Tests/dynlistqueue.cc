59a51c7ee792a10b93f6470310fc94e3d6e5831f
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2017-10-08 18:36:55 -0400

    x1

--- Tests/dynlistqueue.cc
+++ Tests/dynlistqueue.cc
@@ -13,161 +13,131 @@ constexpr size_t N = 17;
 struct SimpleQueue : public Test
 {
   size_t n = 0;
-  DynListQueue<int> s;
+  DynListQueue<int> q;
   SimpleQueue()
   {
     for (size_t i = 0; i < N; ++i, ++n)
-      s.put(i);
+      q.put(i);
   }
 };
 
 struct ComplexQueue : public Test
 {
   size_t n = 0;
-  DynListQueue<DynList<int>> s;
+  DynListQueue<DynList<int>> q;
   ComplexQueue()
   {
     for (size_t i = 0; i < N; ++i, ++n)
-      s.put({ int(i), 0, 1, 2, int(i) });
+      q.put({ int(i), 0, 1, 2, int(i) });
   }
 };
 
 TEST(DynListQueue, empty_queue)
 {
-  DynListQueue<int> s;
-  EXPECT_TRUE(s.is_empty());
-  EXPECT_EQ(s.size(), 0);
-  EXPECT_THROW(s.front(), underflow_error);
-  EXPECT_THROW(s.rear(), underflow_error);
+  DynListQueue<int> q;
+  EXPECT_TRUE(q.is_empty());
+  EXPECT_EQ(q.size(), 0);
+  EXPECT_THROW(q.front(), underflow_error);
+  EXPECT_THROW(q.rear(), underflow_error);
 }
 
-TEST_F(SimpleQueue, push_pop)
+TEST_F(SimpleQueue, get_put)
 {
-  EXPECT_FALSE(s.is_empty());
-  EXPECT_EQ(s.size(), n);
-  EXPECT_EQ(s.(), n - 1);
+  EXPECT_FALSE(q.is_empty());
+  EXPECT_EQ(q.size(), n);
+  EXPECT_EQ(q.front(), 0);
+  EXPECT_EQ(q.rear(), n - 1);
 
   const size_t m = 100;
   for (size_t i = 0; i < m; ++i)
-    EXPECT_EQ(s.push(i), i);
-  EXPECT_EQ(s.size(), n + m);
+    ASSERT_EQ(q.put(i), i);
+  EXPECT_EQ(q.size(), n + m);
+
+  for (size_t i = 0; i < n; ++i)
+    ASSERT_EQ(q.get(), i);
+
+  EXPECT_EQ(q.rear(), m - 1);
+  EXPECT_EQ(q.front(), 0);
+  EXPECT_EQ(q.size(), m);
 
   for (size_t i = 0; i < m; ++i)
-    EXPECT_EQ(s.pop(), m - i - 1);
+    ASSERT_EQ(q.get(), i);
 
-  EXPECT_EQ(s.rear(), n - 1);
-  EXPECT_EQ(s.front(), 0);
-
-  const size_t m = 100;
-  for (size_t i = 0; i < m; ++i)
-    EXPECT_EQ(s.put(i), i);
-  EXPECT_EQ(s.size(), n + m);
+  EXPECT_TRUE(q.is_empty());
+  EXPECT_EQ(q.size(), 0);
 
   for (size_t i = 0; i < m; ++i)
-    EXPECT_EQ(s.get(), i);
-  /*
+    EXPECT_EQ(q.put(i), i);
+  EXPECT_EQ(q.size(), m);
 
-  EXPECT_EQ(s.size(), n);
-
-  for (size_t i = 0; i < m; ++i)
-    EXPECT_EQ(s.push(i), i);
-  EXPECT_EQ(s.size(), n + m);
-
-  for (size_t i = 0; i < m; ++i)
-    EXPECT_EQ(s.pop(), m - i - 1);
-  EXPECT_FALSE(s.is_empty());
-  EXPECT_EQ(s.size(), n);
-
-  s.empty();
-  EXPECT_TRUE(s.is_empty());
-  EXPECT_EQ(s.size(), 0);
-  */
+  q.empty();
+  EXPECT_TRUE(q.is_empty());
+  EXPECT_EQ(q.size(), 0);
 }
-
-/*
-
 
 TEST_F(ComplexQueue, push_pop)
 {
-  EXPECT_FALSE(s.is_empty());
-  EXPECT_EQ(s.size(), n);
-  EXPECT_EQ(s.top().get_first(), n - 1);
-
+  EXPECT_FALSE(q.is_empty());
+  EXPECT_EQ(q.size(), n);
+  EXPECT_EQ(q.rear().get_first(), n - 1);
+  EXPECT_EQ(q.front().get_first(), 0);
+    
   const size_t m = 100;
   for (size_t i = 0; i < m; ++i)
     {
-      EXPECT_EQ(s.push({int(i), 0, 1, int(i)}).get_first(), i);
-      EXPECT_EQ(s.top().get_first(), i);
-      EXPECT_EQ(s.top().get_last(), i);
-      EXPECT_EQ(s.top().nth(1), 0);
-      EXPECT_EQ(s.top().nth(2), 1);
+      ASSERT_EQ(q.put({int(i), 0, 1, int(i)}).get_first(), i);
+      ASSERT_EQ(q.rear().get_first(), i);
+      ASSERT_EQ(q.rear().get_last(), i);
+      ASSERT_EQ(q.rear().nth(1), 0);
+      ASSERT_EQ(q.rear().nth(2), 1);
     }
-  EXPECT_EQ(s.size(), n + m);
+  EXPECT_EQ(q.size(), n + m);
 
-  for (size_t i = 0; i < m; ++i)
+  for (size_t i = 0; i < n; ++i) // extract n items
     {
-      auto l = s.pop();
-      EXPECT_EQ(l.get_first(), m - i - 1);      
-      EXPECT_EQ(l.get_last(), m - i - 1);
-      EXPECT_EQ(l.nth(1), 0);
-      EXPECT_EQ(l.nth(2), 1);
+      auto l = q.get();
+      ASSERT_EQ(l.get_first(), i);      
+      ASSERT_EQ(l.get_last(), i);
+      ASSERT_EQ(l.nth(1), 0);
+      ASSERT_EQ(l.nth(2), 1);
     }
 
-  EXPECT_EQ(s.size(), n);
-
-  for (size_t i = 0; i < m; ++i)
-    {
-      auto & l = s.push({ int(i), 0, 1, int(i) });
-      EXPECT_EQ(l.get_first(), i);
-      EXPECT_EQ(l.get_last(), i);
-      EXPECT_EQ(l.nth(1), 0);
-      EXPECT_EQ(l.nth(2), 1);
-    }
-  EXPECT_EQ(s.size(), n + m);
-
-  for (size_t i = 0; i < m; ++i)
-    {
-      auto l = s.pop();
-      EXPECT_EQ(l.get_first(), m - i - 1);
-      EXPECT_EQ(l.get_last(), m - i - 1);
-      EXPECT_EQ(l.nth(1), 0);
-      EXPECT_EQ(l.nth(2), 1);
-    }
-  EXPECT_EQ(s.size(), n);
+  EXPECT_EQ(q.rear().get_first(), m - 1);
+  EXPECT_EQ(q.rear().get_last(), m - 1);
+  EXPECT_EQ(q.front().get_first(), 0);
+  EXPECT_EQ(q.front().get_last(), 0);
+  EXPECT_EQ(q.size(), m);
   
-  for (size_t i = 0; i < n; ++i)
-    {
-      auto l = s.pop();
-      EXPECT_EQ(l.get_first(), n - i - 1);
-      EXPECT_EQ(l.get_last(), n - i - 1);
-      EXPECT_EQ(l.nth(1), 0);
-      EXPECT_EQ(l.nth(2), 1);
-    }
-  EXPECT_TRUE(s.is_empty());
-  EXPECT_EQ(s.size(), 0);
-
-  EXPECT_TRUE(s.is_empty());
-  EXPECT_EQ(s.size(), 0);
-
   for (size_t i = 0; i < m; ++i)
     {
-      auto & l = s.push({ int(i), 0, 1, int(i) });
+      auto l = q.get();
+      ASSERT_EQ(l.get_first(), i);
+      ASSERT_EQ(l.get_last(), i);
+      ASSERT_EQ(l.nth(1), 0);
+      ASSERT_EQ(l.nth(2), 1);
+    }
+  EXPECT_EQ(q.size(), 0);
+  EXPECT_TRUE(q.is_empty());
+  
+  for (size_t i = 0; i < m; ++i)
+    {
+      auto & l = q.put({ int(i), 0, 1, int(i) });
       EXPECT_EQ(l.get_first(), i);
       EXPECT_EQ(l.get_last(), i);
       EXPECT_EQ(l.nth(1), 0);
       EXPECT_EQ(l.nth(2), 1);
     }
-  EXPECT_EQ(s.size(), m);
+  EXPECT_EQ(q.size(), m);
 
-  s.empty();
-  EXPECT_TRUE(s.is_empty());
-  EXPECT_EQ(s.size(), 0);
+  q.empty();
+  EXPECT_TRUE(q.is_empty());
+  EXPECT_EQ(q.size(), 0);
 }
 
 TEST(DynListQueue, Iterator_on_empty_queue)
 {
-  DynListQueue<int> s;
-  auto it = s.get_it();
+  DynListQueue<int> q;
+  auto it = q.get_it();
   EXPECT_FALSE(it.has_curr());
   EXPECT_THROW(it.get_curr(), overflow_error);
   EXPECT_THROW(it.next(), overflow_error);
@@ -175,122 +145,147 @@ TEST(DynListQueue, Iterator_on_empty_queue)
 
 TEST_F(SimpleQueue, Iterator)
 {
-  auto it = s.get_it(); 
+  auto it = q.get_it(); 
   for (size_t i = 0; it.has_curr(); it.next(), ++i)
-    EXPECT_EQ(it.get_curr(), n - i - 1);
+    ASSERT_EQ(it.get_curr(), i);
 }
 
 TEST_F(ComplexQueue, Iterator)
 {
-  auto it = s.get_it(); 
+  auto it = q.get_it(); 
   for (size_t i = 0; it.has_curr(); it.next(), ++i)
     {
-      EXPECT_EQ(it.get_curr().get_first(), n - i -1);
-      EXPECT_EQ(it.get_curr().get_last(), n - i - 1);
-      EXPECT_EQ(it.get_curr().nth(1), 0);
-      EXPECT_EQ(it.get_curr().nth(2), 1);
+      ASSERT_EQ(it.get_curr().get_first(), i);
+      ASSERT_EQ(it.get_curr().get_last(), i);
+      ASSERT_EQ(it.get_curr().nth(1), 0);
+      ASSERT_EQ(it.get_curr().nth(2), 1);
     }
 }
 
 TEST_F(SimpleQueue, copy_operations)
 {
   { // test copy ctor
-    DynListQueue<int> sc = s;
-    EXPECT_FALSE(sc.is_empty());
-    EXPECT_EQ(sc.size(), n);
-    EXPECT_EQ(s.size(), sc.size());
+    DynListQueue<int> qc = q;
+    EXPECT_FALSE(qc.is_empty());
+    EXPECT_EQ(qc.size(), n);
+    EXPECT_EQ(q.size(), qc.size());
     int i = 0;
     for (; i < n; ++i)
-      EXPECT_EQ(sc.pop(), n - i - 1);
+      ASSERT_EQ(qc.get(), i);
     EXPECT_EQ(i, n);
   }
   
   { // test copy assignment 
-    DynListQueue<int> sc;
-    sc = s;
-    EXPECT_FALSE(sc.is_empty());
-    EXPECT_EQ(s.size(), sc.size());
+    DynListQueue<int> qc;
+    qc = q;
+    EXPECT_FALSE(qc.is_empty());
+    EXPECT_EQ(q.size(), qc.size());
     int i = 0;
     for (; i < n; ++i)
-      EXPECT_EQ(sc.pop(), n - i - 1);
-    EXPECT_EQ(i, s.size());
+      ASSERT_EQ(qc.get(), i);
+    EXPECT_EQ(i, q.size());
   }
 
   // test move ctor
-  DynListQueue<int> sc(move(s));
-  EXPECT_FALSE(sc.is_empty());
-  EXPECT_EQ(sc.size(), n);
-  EXPECT_EQ(s.size(), 0);
-  EXPECT_TRUE(s.is_empty());
+  DynListQueue<int> qc(move(q));
+  EXPECT_FALSE(qc.is_empty());
+  EXPECT_EQ(qc.size(), n);
+  EXPECT_EQ(q.size(), 0);
+  EXPECT_TRUE(q.is_empty());
   int i = 0;
   for (; i < n; ++i)
     {
-      s.push(sc.pop());
-      EXPECT_EQ(s.top(), n - i - 1);
+      q.put(qc.get());
+      ASSERT_EQ(q.rear(), i);
+      ASSERT_EQ(q.front(), 0);
     }
-  EXPECT_EQ(i, n);
-  EXPECT_EQ(s.size(), n);
-  EXPECT_TRUE(sc.is_empty());
 
-  sc = move(s);
-  EXPECT_FALSE(sc.is_empty());
-  EXPECT_EQ(sc.size(), n);
-  EXPECT_TRUE(s.is_empty());
+  EXPECT_EQ(i, n);
+  EXPECT_EQ(q.size(), n);
+  EXPECT_TRUE(qc.is_empty());
+
+  qc = move(q);
+  EXPECT_FALSE(qc.is_empty());
+  EXPECT_EQ(qc.size(), n);
+  EXPECT_TRUE(q.is_empty());
   i = 0;
   for (; i < n; ++i)
     {
-      s.push(sc.pop());
-      EXPECT_EQ(s.top(), i);
+      q.put(qc.get());
+      ASSERT_EQ(q.rear(), i);
+      ASSERT_EQ(q.front(), 0);
     }
   EXPECT_EQ(i, n);
-  EXPECT_EQ(s.size(), n);
-  EXPECT_TRUE(sc.is_empty());
+  EXPECT_EQ(q.size(), n);
+  EXPECT_TRUE(qc.is_empty());
 }
 
 TEST_F(ComplexQueue, copy_operations)
 {
   { // test copy ctor
-    DynListQueue<DynList<int>> sc = s;
-    EXPECT_FALSE(sc.is_empty());
-    EXPECT_EQ(s.size(), sc.size());
-    EXPECT_TRUE(eq(s.top(), sc.top()));
+    DynListQueue<DynList<int>> qc = q;
+    EXPECT_FALSE(qc.is_empty());
+    EXPECT_EQ(q.size(), qc.size());
+    EXPECT_TRUE(eq(q.front(), qc.front()));
+    EXPECT_TRUE(eq(q.rear(), qc.rear()));
   }
   
   { // test copy assignment 
-    DynListQueue<DynList<int>> sc;
-    sc = s;
-    EXPECT_FALSE(sc.is_empty());
-    EXPECT_EQ(s.size(), sc.size());
-    EXPECT_TRUE(eq(s.top(), sc.top()));
+    DynListQueue<DynList<int>> qc;
+    qc = q;
+    EXPECT_FALSE(qc.is_empty());
+    EXPECT_EQ(q.size(), qc.size());
+    EXPECT_TRUE(eq(q.rear(), qc.rear()));
+    EXPECT_TRUE(eq(q.front(), qc.front()));
   }
 
   // test move ctor
-  DynListQueue<DynList<int>> sc(move(s));
-  EXPECT_FALSE(sc.is_empty());
-  EXPECT_EQ(sc.size(), n);
-  EXPECT_EQ(s.size(), 0);
-  EXPECT_TRUE(s.is_empty());
+  DynListQueue<DynList<int>> qc(move(q));
+  EXPECT_FALSE(qc.is_empty());
+  EXPECT_EQ(qc.size(), n);
+  EXPECT_EQ(q.size(), 0);
+  EXPECT_TRUE(q.is_empty());
 
-  EXPECT_EQ(sc.top().get_first(), n - 1);
-  EXPECT_EQ(sc.top().get_last(), n - 1);
-  EXPECT_EQ(sc.top().nth(1), 0);
-  EXPECT_EQ(sc.top().nth(2), 1);
+  int k = 0;
+  for (auto it = qc.get_it(); it.has_curr(); it.next(), ++k)
+    {
+      auto & l = it.get_curr();
+      ASSERT_EQ(l.get_first(), k);
+      ASSERT_EQ(l.get_last(), k);
+      ASSERT_EQ(l.nth(1), 0);
+      ASSERT_EQ(l.nth(2), 1);
+    }
 
-  s = move(sc);
-  EXPECT_FALSE(s.is_empty());
-  EXPECT_EQ(s.size(), n);
-  EXPECT_TRUE(sc.is_empty());
+  EXPECT_EQ(qc.rear().get_first(), n - 1);
+  EXPECT_EQ(qc.rear().get_last(), n - 1);
+  EXPECT_EQ(qc.rear().nth(1), 0);
+  EXPECT_EQ(qc.rear().nth(2), 1);
 
-  EXPECT_EQ(s.top().get_first(), n - 1);
-  EXPECT_EQ(s.top().get_last(), n - 1);
-  EXPECT_EQ(s.top().nth(1), 0);
-  EXPECT_EQ(s.top().nth(2), 1);
+  q = move(qc);
+  EXPECT_FALSE(q.is_empty());
+  EXPECT_EQ(q.size(), n);
+  EXPECT_TRUE(qc.is_empty());
+
+  k = 0;
+  for (auto it = qc.get_it(); it.has_curr(); it.next(), ++k)
+    {
+      auto & l = it.get_curr();
+      ASSERT_EQ(l.get_first(), k);
+      ASSERT_EQ(l.get_last(), k);
+      ASSERT_EQ(l.nth(1), 0);
+      ASSERT_EQ(l.nth(2), 1);
+    }
+
+  EXPECT_EQ(q.rear().get_first(), n - 1);
+  EXPECT_EQ(q.rear().get_last(), n - 1);
+  EXPECT_EQ(q.rear().nth(1), 0);
+  EXPECT_EQ(q.rear().nth(2), 1);
 }
 
 TEST_F(SimpleQueue, traverse)
 {
   size_t i = 0;
-  bool ret = s.traverse([&i, N = n] (int k) { return (k == N - i++ - 1); });
+  bool ret = q.traverse([&i] (int k) { return (k == i++); });
   EXPECT_TRUE(ret);
   EXPECT_EQ(i, n);
 }
@@ -298,16 +293,15 @@ TEST_F(SimpleQueue, traverse)
 TEST_F(ComplexQueue, traverse)
 {
   int i = 0;
-  auto ret = s.traverse([&i, N = n] (const DynList<int> & l)
+  auto ret = q.traverse([&i] (const DynList<int> & l)
 			{
-			  return l.get_first() == N - i - 1 and
-			  l.get_last() == N - i++ - 1 and
+			  return l.get_first() == i and
+			  l.get_last() == i++ and
 			  l.nth(1) == 0 and l.nth(2) == 1;
 			});
   EXPECT_TRUE(ret);
   EXPECT_EQ(i, n);
 }
 
-*/
 
 
