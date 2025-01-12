5917ff64f75efac264c15da461f19476dfa547b2
Leandro Rabindranath Leon <leandro.leon@geniussports.com>
2025-01-12 14:05:59 -0500

    refactor: Implement in-place multisort for arrays
    Refactor the sort functions to correctly implement the
    in-place multisort operation using indices rather than
    pairs.  This allows sorting multiple arrays according to
    one array's ordering. Add `to_vector` helper to tpl_array.
    Also, add `to_string` to ah-string-utils to easily
    serialize the content of data structures.

--- Tests/sort_arrays.cc
+++ Tests/sort_arrays.cc
@@ -2,8 +2,16 @@
 // Created by lrleon on 1/6/25.
 //
 
+# include <vector>
+# include <sstream>
+# include <array>
+# include <algorithm>
+# include <functional>
+# include <iostream>
+# include <ranges>
 # include <gtest/gtest.h>
 
+# include <ah-string-utils.H>
 # include <ahSort.H>
 
 using namespace Aleph;
@@ -14,9 +22,18 @@ struct SimpleArray : public Test
 {
   vector<int> a = { 3, 2, 1, 0, 4, 5, 6, 7, 8, 9 };
   Array<int> b = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+  Array<string> c = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" };
 };
 
 TEST_F(SimpleArray, sort)
 {
-  in_place_multisort<std::less<int>>(a, b);
+  in_place_multisort_arrays(std::less<int>(), a, b, c);
+
+  ASSERT_EQ(a, vector<int>({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
+
+  ASSERT_EQ(to_vector(b), vector<int>({ 3, 2, 1, 0, 4, 5, 6, 7, 8, 9 }));
+
+  ASSERT_EQ(to_vector(c),
+    vector<string>({"d", "c", "b", "a", "e", "f", "g", "h", "i", "j"}));
+
 }
