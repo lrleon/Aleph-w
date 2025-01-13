76a8151c73e21b36dd04097a1d5164665f2c9c69
Leandro Rabindranath Leon <leandro.leon@geniussports.com>
2025-01-13 12:55:15 -0500

    refactor: Rename to_vector to to_stdvector
    The function to convert a container to a std::vector was
    renamed to to_stdvector to avoid naming conflicts and
    to be more explicit about the return type.

--- Tests/sort_arrays.cc
+++ Tests/sort_arrays.cc
@@ -31,9 +31,9 @@ TEST_F(SimpleArray, sort)
 
   ASSERT_EQ(a, vector<int>({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
 
-  ASSERT_EQ(to_vector(b), vector<int>({ 3, 2, 1, 0, 4, 5, 6, 7, 8, 9 }));
+  ASSERT_EQ(to_stdvector(b), vector<int>({ 3, 2, 1, 0, 4, 5, 6, 7, 8, 9 }));
 
-  ASSERT_EQ(to_vector(c),
+  ASSERT_EQ(to_stdvector(c),
     vector<string>({"d", "c", "b", "a", "e", "f", "g", "h", "i", "j"}));
 
 }
