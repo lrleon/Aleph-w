3be8020b7f125eb72ddc82b71904f367313829b8
Leandro Rabindranath Leon <leandro.r.leon@gmail.com>
2026-02-08 21:42:31 -0500

    feat(Fenwick): add Fenwick tree implementation and example

--- Examples/fenwick_tree_example.cc
+++ Examples/fenwick_tree_example.cc
@@ -1,3 +1,33 @@
+/*
+Aleph_w
+
+  Data structures & Algorithms
+  version 2.0.0b
+  https://github.com/lrleon/Aleph-w
+
+  This file is part of Aleph-w library
+
+  Copyright (c) 2002-2026 Leandro Rabindranath Leon
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
 /**
  * @file fenwick_tree_example.cc
  * @brief Fenwick trees applied to trading and sports betting
@@ -34,6 +64,7 @@
  * ./fenwick_tree_example
  */
 
+# include <cstdio>
 # include <iostream>
 # include <iomanip>
 # include <string>
